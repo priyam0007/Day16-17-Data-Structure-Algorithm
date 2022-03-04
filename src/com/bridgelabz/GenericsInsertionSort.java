bc4321c2af6bc4ad8dc0229bc71876b6e5f912ce
Priya <Priyamadiwalar0007@gmail.com>
2022-03-05 00:07:28 +0530

    Added:Algorithm Programs

---
+++ src/com/bridgelabz/GenericsInsertionSort.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+
+public class GenericsInsertionSort {
+	public static void main(String[] args) {
+
+		InsertionSort insertion = new InsertionSort();
+		String array[] = { "hi", "guyz", "hello", "welcome", "to", "game" };
+
+		insertion.insertionSort(array);
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+	public <K extends Comparable<K>> K insertionSort(K[] array) {
+
+		for (int i = 0; i < array.length; i++) {
+			K temp = array[i];
+			int index = i - 1;
+			while (index >= 0 && array[index].compareTo(temp) > 0) {
+				array[index + 1] = array[index];
+				index--;
+			}
+
+			array[index + 1] = temp;
+
+		}
+		return null;
+	}
+}
